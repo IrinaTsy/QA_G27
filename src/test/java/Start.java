e1d5f1f3c0521b869a72f42857e7f158d90c9a9f
IrinaTsy <112385684+IrinaTsy@users.noreply.github.com>
2024-06-16 16:17:16 +0300

    Start

---
+++ src/test/java/Start.java
@@ -0,0 +1,17 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Start {
+
+  @Test
+  void SelenideSearchTest(){
+
+    open("https://www.google.com");
+    $("[name=q]").setValue("selenide").pressEnter();
+    $("[id=search]").shouldHave(text("https://selenide.org"));
+  }
+}
